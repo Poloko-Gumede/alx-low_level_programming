10137caaf4026f243fc0140978a620c09b5a26c7
Poloko Gumede <gumedepoloko@gmail.com>
2023-02-17 09:05:02 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 /*prints a string*/
 
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main(void) {
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
 }
+
