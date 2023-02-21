e5cc62d9522507785ea450dc6bcc7a986ce3c649
Poloko Gumede <gumedepoloko@gmail.com>
2023-02-21 12:39:04 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,29 +6,10 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
 return (write(1, &c, 1));
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
 }
 
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints "_putchar", followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c[] = "_putchar\n";
+int i;
+
+for (i = 0; i < 9; i++)
+{
+_putchar(c[i]);
+}
+return (0);
+}
+
