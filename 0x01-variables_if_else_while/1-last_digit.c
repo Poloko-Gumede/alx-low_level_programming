cd7eb35b8df94fe700536b365f0258f4f4eab827
Poloko Gumede <gumedepoloko@gmail.com>
2023-02-23 01:36:13 -0800

    0x03-debugging

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,6 +15,9 @@ int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
+if (last_digit < 0)
+last_digit *= -1;
+
 printf("Last digit of %d is ", n);
 
 if (n < 0)
@@ -25,9 +28,9 @@ last_digit = n % 10;
 if (last_digit == 0)
 printf("%d and is 0\n", last_digit);
 else if (last_digit < 6)
-printf("%d and is less than 6 and not 0\n", -last_digit);
+printf("%d and is less than 6 and not 0\n", last_digit);
 else
-printf("%d and is greater than 5\n", last_digit);
+printf("%d and is less than 6 and not 0\n", last_digit);
 
 return (0);
 }
