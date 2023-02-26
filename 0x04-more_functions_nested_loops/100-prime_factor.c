2e2d3598c4fe720009d58a8dd1f455bcd89a5173
Poloko Gumede <gumedepoloko@gmail.com>
2023-02-26 15:50:24 -0800

    100-prime_factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -13,24 +13,26 @@ int main(void)
 unsigned long n = 612852475143;
 unsigned long largest_factor = 0;
 
-while (n % 2 == 0) {
+while (n % 2 == 0)
+{
 largest_factor = 2;
 n /= 2;
 }
 
-for (int i = 3; i <= sqrt(n); i += 2) {
-while (n % i == 0) {
+for (int i = 3; i <= sqrt(n); i += 2)
+{
+while (n % i == 0)
+{
 largest_factor = i;
 n /= i;
 }
 }
 
-if (n > 2) {
+if (n > 2)
 largest_factor = n;
-}
 
 printf("%lu\n", largest_factor);
 
-return 0;
+return (0);
 }
 
