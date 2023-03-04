a749fc0944ea421dc5d16a010cc523fe6eaf333d
Poloko Gumede <gumedepoloko@gmail.com>
2023-03-04 06:01:19 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,9 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
@@ -15,28 +17,3 @@ c++;
 
 _putchar('\n');
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-print_alphabet();
-
-return (0);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 int _islower(int c);
 int _putchar(char c);
 int _isalpha(int c);
+void print_alphabet(void);
 
 #endif /* MAIN_H */
