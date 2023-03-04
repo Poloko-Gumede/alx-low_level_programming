ac880bd3e403e9b512b197de950bbf9894310583
Poloko Gumede <gumedepoloko@gmail.com>
2023-03-04 03:14:12 -0800

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,22 +8,21 @@
 int main(void)
 {
 int i, j;
+
 for (i = 0; i < 9; i++)
 {
 for (j = i + 1; j < 10; j++)
 {
 putchar(i + '0');
-putchar(',');
-putchar(' ');
 putchar(j + '0');
-if (i < 8)
+if (i != 8 || j != 9)
 {
-putchar(',');
-putchar(' ');
+putchar(44);
+putchar(32);
 }
 }
 }
-putchar('\n');
+putchar(10);
 
 return (0);
 }
